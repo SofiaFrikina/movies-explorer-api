d1c7936079602b09de9e9f15b9fa2eafd599fc53
SofiaFrikina <sofi.frikina@yandex.ru>
2023-12-18 21:07:44 +0300

    поменяла id на movieId

--- controllers/movies.js
+++ controllers/movies.js
@@ -28,7 +28,7 @@ const createMovie = (req, res, next) => {
 };
 
 const deleteMovie = (req, res, next) => {
-  Movie.findById(req.params.id)
+  Movie.findById(req.params.movieId)
     .then((movie) => {
       if (!movie) {
         throw new NotFoundError('Фильм не найден');
